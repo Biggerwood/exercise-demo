26ffda630d8d7311b6c2b0e7fc48c5e0906f3939
Biggerwood <15933513586@163.com>
2016-05-26 09:23:47 +0800

    利用localStorage保存爱好设置

--- memo/record.js
+++ memo/record.js
@@ -37,6 +37,43 @@ var jump = function(){
     var hash = location.hash;
     document.body.className = hash.substr(1);
 }
+var loadSet = function(){
+    if('localStorage' in window){
+        var name = localStorage.getItem("name"),
+            prefer = localStorage.getItem("prefer"),
+            oh1 = $('h1')[0],
+            oPrefer = $('#color')[0],
+            oName = $('#name')[0];
+        if(name){
+            oh1.innerHTML = name + "'s Tasks";
+            oName.value = name;
+        }else{
+            oh1.innerHTML = "My Tasks";
+            oName.value = '';
+        }
+        if(prefer){
+            oh1.className = prefer;
+            oPrefer.value = prefer;
+        }else{
+            oh1.className = 'blue';
+            oPrefer.value = 'blue';
+        }
+    }
+}
+var saveSet = function(e){
+    e.preventDefault();
+    event.returnValue = false;
+    if('localStorage' in window){
+        var name =  $('#name')[0].value,
+            prefer = $('#color')[0].value;
+        localStorage.setItem('name', name);
+        localStorage.setItem('prefer', prefer);
+        loadSet();
+    }
+}
 addHandler(window, "load", function(){
-    addHandler(window, "hashchange", jump)    
+    loadSet();
+    var oSave = $('#save')[0];
+    addHandler(window, "hashchange", jump);
+    addHandler(oSave, 'click', saveSet);    
 });
